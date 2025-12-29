3d4a316ab91151df20037429e32aa5dffbe0d163
andre00bejarano00vaca <andre.bejarano.vaca@gmail.com>
2025-12-29 10:50:40 -0400

    token

--- src/lib/graphql-client.ts
+++ src/lib/graphql-client.ts
@@ -1,3 +1,5 @@
+import { getAccessToken } from "@/service/auth.service";
+
 /**
  * Este es el motor central de tu aplicación. 
  * Todos los servicios que hemos creado (Recaudación, PEI, etc.) 
@@ -6,11 +8,12 @@
 export async function fetchGraphQL<T>(query: string, variables = {}): Promise<T> {
   // Asegúrate de configurar esta variable en tu archivo .env
   const API_URL = process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://172.21.2.20/planning";
-
+  const token = getAccessToken();
   const response = await fetch(API_URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      'Authorization': token ? `Bearer ${token}` : '',
       // Aquí podrías agregar el token de autenticación si fuera necesario
     },
     body: JSON.stringify({ query, variables }),
